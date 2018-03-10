9a4987509641b473a3528d0b3346ad0e24cb72c2
Maxwell Cady <maxwell.josef@gmail.com>
2018-03-10 12:44:52 -0600

    initial setup in place with react and webpack

--- README.md
+++ README.md
@@ -1,13 +1,1 @@
-# React-starter
-Very minimal starter for React applications
 
-## Setup for dev
-npm install
-npm run dev
-
-## Build
-npm install
-npm run build
-
-
-Based loosely on https://medium.freecodecamp.org/learn-webpack-for-react-a36d4cac5060
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,34 +1,10 @@
 import React from 'react';
-import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
-import importedComponent from 'react-imported-component';
-
-import Home from '../../pages/Home';
-import Loading from '../Loading/Loading';
 
 import './App.scss';
 
-const AsyncDynamicPage = importedComponent(
-  () => import(/* webpackChunkName:'DynamicPage' */ '../../pages/DynamicPage'),
-  {
-    LoadingComponent: Loading
-  }
-);
-const AsyncNoMatch = importedComponent(
-  () => import(/* webpackChunkName:'NoMatch' */ '../../pages/NoMatch'),
-  {
-    LoadingComponent: Loading
-  }
-);
-
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={ Home } />
-        <Route exact path="/dynamic" component={ AsyncDynamicPage } />
-        <Route component={ AsyncNoMatch } />
-      </Switch>
-    </Router>
+    <div>App works!</div>
   );
 }
 
