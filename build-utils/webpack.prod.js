bf013a37be732369309dd11e061bd9740c1daac8
Maxwell Cady <maxwell.josef@gmail.com>
2018-03-11 19:10:16 -0500

    clean up comments

--- build-utils/webpack.prod.js
+++ build-utils/webpack.prod.js
@@ -11,8 +11,6 @@ module.exports = {
     app: [`${commonPaths.appEntry}/index.js`]
   },
   output: {
-    // We want to create the JavaScript bundles under a
-    // 'static' directory
     filename: 'static/[name].[hash].js',
     publicPath: './'
   },
@@ -36,10 +34,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        // We configure 'Extract Text Plugin'
         use: ExtractTextPlugin.extract({
-          // loader that should be used when the
-          // CSS is not extracted
           fallback: 'style-loader',
           use: [
             {
@@ -51,15 +46,10 @@ module.exports = {
                 // to @import(ed) resources
                 importLoaders: 1,
                 camelCase: true,
-                // Create source maps for CSS files
                 sourceMap: true
               }
             },
             {
-              // PostCSS will run before css-loader and will
-              // minify and autoprefix our CSS rules. We are also
-              // telling it to only use the last 2
-              // versions of the browsers when autoprefixing
               loader: 'postcss-loader',
               options: {
                 config: {
@@ -111,7 +101,6 @@ module.exports = {
     ]
   },
   plugins: [
-    // Create the stylesheet under 'styles' directory
     new ExtractTextPlugin({
       filename: 'styles/styles.[contenthash].css',
       allChunks: true
--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,6 @@ const webpackMerge = require('webpack-merge');
 const buildValidations = require('./build-utils/build-validations');
 const commonConfig = require('./build-utils/webpack.common');
 
-// 'env' will contain the environment variable from 'scripts'
-// section in 'package.json'.
-// console.log(env); => { env: 'dev' }
 module.exports = env => {
 
   // We use 'buildValidations' to check for the 'env' flag
@@ -13,19 +10,12 @@ module.exports = env => {
     throw new Error(buildValidations.ERR_NO_ENV_FLAG);
   }
 
-  // Select which Webpack configuration to use; development
-  // or production
-  // console.log(env.env); => dev
   const envConfig = require(`./build-utils/webpack.${env.env}.js`);
 
-  // 'webpack-merge' will combine our shared configurations, the
-  // environment specific configurations and any addons we are
-  // including
   const mergedConfig = webpackMerge(
     commonConfig,
     envConfig
   );
 
-  // Then return the final configuration for Webpack
   return mergedConfig;
 };
